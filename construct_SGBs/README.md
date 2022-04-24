6459f315e3da27ba80bd851233e8db5113f2e4c5
Matthew R. Hayward <mrh24@login06.o2.rc.hms.harvard.edu>
2022-04-24 13:32:22 -0400

    makeSGBs

---
+++ construct_SGBs/README.md
@@ -0,0 +1,6 @@
+Build Species Genome Bins (SGBs)
+
+## Overview
+Species genome binning is a method by which genomes are grouped by genetic similarity so as to approximate a classically defined bacterial species.
+
+## Content
---
+++ construct_SGBs/SGB_generation.R
@@ -0,0 +1,16 @@
+#Produce SGBs (species genome bins) through cluster of ANI (average nucleotide identity; MASH v2.1.1) 
+#between genomes and cutting the resulting dendrogram at a 5% ANI
+
+library(fastcluster)
+
+#MASH table; 2.8GB so not provided here. The scripts to generate this table are in the "mash_table" directory
+mashDists = read.table("mashDistances_26Nov2019.tab", header = T, row.names = 1, check.names = F)
+dists = as.dist(mashDists)
+
+clustered = hclust(dists, method = "average")
+tree_cut_005 = cutree(clustered, h = 0.05)
+countSGB = table(tree_cut_005)
+
+SGB_tab = cbind(names(tree_cut_005), as.character(tree_cut_005))
+colnames(SGB_tab) = c("sequence", "SGB_num")
+#write.table(SGB_tab, sep = "\t", quote = F, file = "SGB_assignment_26Nov2019.tab", row.names = F, col.names = F)

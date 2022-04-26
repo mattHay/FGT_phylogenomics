d63eef5b7ba354b45b3b5b276201888eac558966
Matthew R. Hayward <mrh24@compute-a-16-164.o2.rc.hms.harvard.edu>
2022-04-25 20:48:31 -0400

    gtdb

---
+++ assign_taxonomy/gtdb-tk/summariseResults.py
@@ -0,0 +1,30 @@
+#Summarise GTDB-Tk results
+import glob
+
+count = 0
+
+output = open("gtdb-tk_classification.tab" ,"w")
+
+genome_to_SGB ={}
+for ent in open("SGB_assignment_REASIGNED_AFTER_COLLAPSE.tab", "r"):
+    ent = ent.rstrip().split("\t")
+    genome_to_SGB[ent[0]] = ent[1]
+
+for ent in glob.glob("gtdb-tk_out/*"):
+
+    samp = ent.split("/")[1].split(".gtdb")[0]
+    count = 0
+
+    for line in open(ent, "r"):
+        line = line.rstrip()
+    
+        if count  < 1:
+            count += 1
+        
+        else:
+            line = line.split("\t")
+            genome = line[0].split("-FIN")[0].split(".fasta")[0]
+            
+            output.write(genome_to_SGB[genome] + "\t" + line[1] + "\n")
+
+output.close()
